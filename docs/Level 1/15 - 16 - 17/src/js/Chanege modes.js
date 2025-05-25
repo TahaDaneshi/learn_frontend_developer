e3990e216de71e992cfd4e3a48719478f3d2cb6a
TahaDaneshi <mttahadaneshi1384mt@gmail.com>
2025-05-25 14:26:19 +0330

    Added scripts oboute automatic change of background color of 'nav'

--- docs/Level 1/15 - 16 - 17/src/js/Chanege modes.js
+++ docs/Level 1/15 - 16 - 17/src/js/Chanege modes.js
@@ -1,1 +1,6 @@
 'strict'
+const nav = document.getElementsByTagName('nav')[0]
+
+setInterval(function () {
+  nav.setAttribute('style', `background-color:rgba(${Math.random() * 255}, ${Math.random() * 255}, 250, 0.5);`)
+}, 6000)
