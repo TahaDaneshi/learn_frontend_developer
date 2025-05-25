bb1e07b15fa295e29d1066d8c9e19040376a8933
TahaDaneshi <mttahadaneshi1384mt@gmail.com>
2025-05-25 15:03:15 +0330

    Optimized setInterval

--- docs/Level 1/15 - 16 - 17/src/js/Chanege modes.js
+++ docs/Level 1/15 - 16 - 17/src/js/Chanege modes.js
@@ -1,6 +1,9 @@
 'strict'
 const nav = document.getElementsByTagName('nav')[0]
 
-setInterval(function () {
-  nav.setAttribute('style', `background-color:rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.5);transition: background-color 3s ease;`)
-}, 5500)
+async function auto() {
+  setInterval(function () {
+    nav.setAttribute('style', `background-color:rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.5);transition: background-color 3s ease;`)
+  }, 5500)
+}
+auto()
