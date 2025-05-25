129dcecf943cf8b3e78166705c585641b14d1294
TahaDaneshi <mttahadaneshi1384mt@gmail.com>
2025-05-25 14:44:05 +0330

    Completing the background scripts

--- docs/Level 1/15 - 16 - 17/src/js/Chanege modes.js
+++ docs/Level 1/15 - 16 - 17/src/js/Chanege modes.js
@@ -2,5 +2,5 @@
 const nav = document.getElementsByTagName('nav')[0]
 
 setInterval(function () {
-  nav.setAttribute('style', `background-color:rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 240, 0.5);`)
-}, 6000)
+  nav.setAttribute('style', `background-color:rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.5);transition: background-color 3s ease;`)
+}, 5500)
