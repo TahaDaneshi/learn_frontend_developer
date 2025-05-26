3f60635afea3f1a0b286b3d07a5a9eac124598c2
TahaDaneshi <mttahadaneshi1384mt@gmail.com>
2025-05-26 10:04:34 +0330

    JavaScript code change mode

--- docs/Level 1/15 - 16 - 17/src/js/Change modes.js
+++ docs/Level 1/15 - 16 - 17/src/js/Change modes.js
@@ -1,10 +1,15 @@
 'strict'
 
 // ELEMENTS
+const HTML = document.getElementById("root");
 const nav = document.getElementsByTagName('nav')[0];
 // changers
+// language mode changer
 const btnChangeLanguage = document.getElementById('btn-change-language');
 const txtCurrentLanguage = document.getElementById('txt-current-language');
+// dark mode changer
+const btnChangeMode = document.getElementById("btn-change-mode");
+const imgCurrentMode = document.getElementById("img-current-mode");
 // sections
 const EnSection = document.getElementById("EN");
 const FaSection = document.getElementById("FA");
@@ -13,7 +18,7 @@ async function auto() {
   // change 'nav' background color 
   setInterval(function () {
     nav.setAttribute('style', `background-color:rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.5);transition: background-color 3s ease;`)
-  }, 5500)
+  }, 5500);
 }
 auto()
 
@@ -27,4 +32,12 @@ btnChangeLanguage.addEventListener('click', () => {
   };
   EnSection.classList.toggle("none")
   FaSection.classList.toggle("none")
+})
+btnChangeMode.addEventListener("click", function () {
+  if (HTML.classList.contains("dark")) {
+    imgCurrentMode.setAttribute("src", "../../../learn images/Icons/Objects/Sun.svg");
+  } else {
+    imgCurrentMode.setAttribute("src", "../../../learn images/Icons/Objects/Moon.svg");
+  }
+  HTML.classList.toggle("dark")
 })
