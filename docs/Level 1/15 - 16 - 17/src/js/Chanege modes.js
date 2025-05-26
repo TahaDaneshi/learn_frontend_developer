85297ae2e625ddad8c5e26a853ffdd1509671be8
TahaDaneshi <mttahadaneshi1384mt@gmail.com>
2025-05-26 09:10:06 +0330

    Divided 'Chanege modes.js' into several parts

--- docs/Level 1/15 - 16 - 17/src/js/Chanege modes.js
+++ docs/Level 1/15 - 16 - 17/src/js/Chanege modes.js
@@ -1,9 +1,15 @@
 'strict'
+
+// ELEMENTS
 const nav = document.getElementsByTagName('nav')[0]
 
+// FUNCTIONS
 async function auto() {
+  // change 'nav' background color 
   setInterval(function () {
     nav.setAttribute('style', `background-color:rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.5);transition: background-color 3s ease;`)
   }, 5500)
 }
 auto()
+
+// EVENTS
